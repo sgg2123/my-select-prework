021c86a211050fdcc792e3b0ac82c74f3df25a83
Sydney Garay <sgg2123@columbia.edu>
2018-05-02 15:00:48 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  i= 0
+  new_array = []
+  while i < array.length
+      if yield(array[i]) 
+        new_array << array[i]
+      end
+      i = i + 1
+    end
+  new_array
 end
