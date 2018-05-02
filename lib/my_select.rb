b56007c12d2069e828f6242665fd3ab9b45257f4
Sydney Garay <sgg2123@columbia.edu>
2018-05-02 15:00:03 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,8 @@ def my_select(array)
   i= 0
   new_array = []
   while i < array.length
-      new_array << yield(array[i]) 
+      if yield(array[i]) 
+        new_array << array
       i = i + 1
     end
   new_array
